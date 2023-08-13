9d7b277b8ae8ede91083ee322c3aa9aa57ad6ebd
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-08-12 23:05:21 -0600

    New relationship for multi-tenantcy

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Owner extends Model
@@ -13,5 +14,10 @@ class Owner extends Model
     public function pets(): HasMany
     {
         return $this->hasMany(Pet::class);
+    }
+
+    public function clinic(): BelongsToMany
+    {
+        return $this->belongsToMany(Clinic::class);
     }
 }
