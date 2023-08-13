22db1420f075678753ea9fb4f28c54fa432152de
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-08-12 23:12:17 -0600

    new migrations

---
+++ database/migrations/2023_08_12_202853_create_clinic_pet_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Clinic;
+use App\Models\Pet;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clinic_pet', function (Blueprint $table) {
+            $table->foreignIdFor(Clinic::class);
+            $table->foreignIdFor(Pet::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clinic_pet');
+    }
+};
---
+++ database/migrations/2023_08_12_203531_create_slots_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->time('start');
+            $table->time('end');
+            $table->string('status');
+            $table->foreignIdFor(User::class, 'owner_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('slots');
+    }
+};
