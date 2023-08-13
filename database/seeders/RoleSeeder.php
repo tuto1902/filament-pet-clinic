d7232d8359e959dc72107ce2bd76910425f48092
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-08-12 23:10:44 -0600

    New database seeders

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::factory(4)
+            ->state(new Sequence(
+                ['name' => 'admin', 'description' => 'Admin User'],
+                ['name' => 'doctor', 'description' => 'Pet Doctor'],
+                ['name' => 'owner', 'description' => 'Pet Owner'],
+                ['name' => 'staff', 'description' => 'Clinic Staff'],
+            ))
+            ->create();
+    }
+}
