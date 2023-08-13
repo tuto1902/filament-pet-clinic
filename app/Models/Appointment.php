2f6b8b154d5679d190594fcf5970ac3b6a9f0514
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-08-12 23:00:57 -0600

    new relationsthips

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,6 +6,7 @@ use App\Enums\AppointmentStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Appointment extends Model
 {
@@ -18,5 +19,15 @@ class Appointment extends Model
     public function pet(): BelongsTo
     {
         return $this->belongsTo(Pet::class);
+    }
+
+    public function clinic(): BelongsToMany
+    {
+        return $this->belongsToMany(Clinic::class);
+    }
+
+    public function slot(): BelongsTo
+    {
+        return $this->belongsTo(Slot::class);
     }
 }
