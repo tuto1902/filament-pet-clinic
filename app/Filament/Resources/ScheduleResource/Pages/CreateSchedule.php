af5d7e107ec07f505e23109bab66de1b4d182466
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-09-05 10:08:23 -0600

    ScheduleResource pages

---
+++ app/Filament/Resources/ScheduleResource/Pages/CreateSchedule.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\ScheduleResource\Pages;
+
+use App\Filament\Resources\ScheduleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSchedule extends CreateRecord
+{
+    protected static string $resource = ScheduleResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
