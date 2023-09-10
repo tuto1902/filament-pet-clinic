dd9e492cb72d7b322fd63bea1424b49545de4632
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-09-10 14:33:50 -0600

    updated database seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::whereName('admin')->first();
+
+        User::factory()->for($adminRole)->create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'phone' => '5555551234'
+        ]);
+    }
+}
