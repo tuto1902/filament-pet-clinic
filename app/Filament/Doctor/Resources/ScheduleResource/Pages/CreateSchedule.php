0988afdfde4364185a9ab5f73002bf296a7210ff
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-09-10 14:27:52 -0600

    Schedule resource pages

---
+++ app/Filament/Doctor/Resources/ScheduleResource/Pages/CreateSchedule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Doctor\Resources\ScheduleResource\Pages;
+
+use App\Filament\Doctor\Resources\ScheduleResource;
+use Filament\Actions;
+use Filament\Facades\Filament;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSchedule extends CreateRecord
+{
+    protected static string $resource = ScheduleResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['owner_id'] = Filament::auth()->user()->id;
+
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
