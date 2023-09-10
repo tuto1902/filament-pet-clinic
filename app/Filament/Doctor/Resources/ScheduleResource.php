1bb495a221eecc4d784d6a6288302115ff81e8e4
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-09-10 14:27:22 -0600

    New schedule resource

---
+++ app/Filament/Doctor/Resources/ScheduleResource.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Filament\Doctor\Resources;
+
+use App\Filament\Doctor\Resources\ScheduleResource\Pages;
+use App\Filament\Doctor\Resources\ScheduleResource\RelationManagers;
+use App\Models\Schedule;
+use App\Models\Slot;
+use Filament\Facades\Filament;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ScheduleResource extends Resource
+{
+    protected static ?string $model = Schedule::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-clock';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Forms\Components\Section::make([
+                Forms\Components\DatePicker::make('date')
+                    ->native(false)
+                    ->closeOnDateSelection()
+                    ->required(),
+                Forms\Components\Repeater::make('slots')
+                    ->relationship()
+                    ->schema([
+                        Forms\Components\TimePicker::make('start')
+                            ->seconds(false)
+                            ->required(),
+                        Forms\Components\TimePicker::make('end')
+                            ->seconds(false)
+                            ->required()
+                    ])
+            ])
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->groups([
+                Tables\Grouping\Group::make('date')
+                    ->collapsible()
+                    ->getTitleFromRecordUsing(fn (Schedule $record) => $record->date->format('M d, Y'))
+            ])
+            ->defaultGroup('date')
+            ->groupsInDropdownOnDesktop()
+            ->columns([
+                Tables\Columns\TextColumn::make('date')
+                    ->date('M d, Y')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('slots')
+                    ->badge()
+                    ->formatStateUsing(fn (Slot $state) => $state->formatted_time),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSchedules::route('/'),
+            'create' => Pages\CreateSchedule::route('/create'),
+            'edit' => Pages\EditSchedule::route('/{record}/edit'),
+        ];
+    }    
+}
