ef179d37cd66be890bc939573b1b6738e2c65972
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-09-17 23:53:10 -0600

    clinic model

---
+++ app/Models/Clinic.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Clinic extends Model
+{
+    use HasFactory;
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function pets(): BelongsToMany
+    {
+        return $this->belongsToMany(Pet::class);
+    }
+
+    public function schedules(): HasMany
+    {
+        return $this->hasMany(Schedule::class);
+    }
+
+    public function appointments(): HasMany
+    {
+        return $this->hasMany(Appointment::class);
+    }
+}
