62ae61d81ac677b5848fb50c9da3b8cea0cb8547
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-09-17 23:55:27 -0600

    database migrations for new tables and updates

---
+++ database/migrations/2023_09_13_233220_create_clinic_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Clinic;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clinic_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Clinic::class);
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clinic_user');
+    }
+};
