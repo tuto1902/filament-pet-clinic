2eea8584c459b4ec45cfaee8d4c139c3fa5ff68c
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-09-17 23:54:51 -0600

    larastan error fix

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
+use App\Models\User;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -15,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        UserPolicy::class
+        User::class => UserPolicy::class
     ];
 
     /**
