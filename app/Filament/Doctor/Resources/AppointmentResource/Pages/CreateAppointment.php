77b9d756fa1a6bf73c304c37aa27ecc5a231e676
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-10-02 10:27:15 -0600

    Updates to the CreateAppointment page for the Doctor panel

---
+++ app/Filament/Doctor/Resources/AppointmentResource/Pages/CreateAppointment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Doctor\Resources\AppointmentResource\Pages;
+
+use App\Filament\Doctor\Resources\AppointmentResource;
+use Filament\Actions;
+use Filament\Facades\Filament;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAppointment extends CreateRecord
+{
+    protected static string $resource = AppointmentResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['doctor_id'] = Filament::auth()->user()->id;
+
+        return $data;
+    }
+}
