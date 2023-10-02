fbef3f448c01621062046d07967160c0202406bf
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-10-02 10:25:59 -0600

    PetResource for the Doctor panel

---
+++ app/Filament/Doctor/Resources/PetResource.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Filament\Doctor\Resources;
+
+use App\Filament\Doctor\Resources\PetResource\Pages;
+use App\Filament\Doctor\Resources\PetResource\RelationManagers;
+use App\Models\Pet;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Storage;
+
+class PetResource extends Resource
+{
+    protected static ?string $model = Pet::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-heart';
+
+    protected static ?int $navigationSort = 3;
+
+    protected static ?string $tenantOwnershipRelationshipName = 'clinics';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make([
+                    Forms\Components\FileUpload::make('avatar')
+                        ->image()
+                        ->imageEditor(),
+                    Forms\Components\TextInput::make('name')
+                        ->required(),
+                    Forms\Components\DatePicker::make('date_of_birth')
+                        ->native(false)
+                        ->required()
+                        ->closeOnDateSelection()
+                        ->displayFormat('M d Y'),
+                    Forms\Components\Select::make('type')
+                        ->native(false)
+                        ->options(PetType::class),
+                    Forms\Components\Select::make('owner_id')
+                        ->relationship('owner', 'name')
+                        ->native(false)
+                        ->searchable()
+                        ->preload()
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('avatar')
+                    ->circular(),
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('type')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('date_of_birth')
+                    ->date('M d Y')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('owner.name')
+                    ->sortable()
+                    ->searchable()
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+                    ->before(function (Pet $record) {
+                        // Delete file
+                        Storage::delete('public/' . $record->avatar);
+                    })
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPets::route('/'),
+            'create' => Pages\CreatePet::route('/create'),
+            'edit' => Pages\EditPet::route('/{record}/edit'),
+        ];
+    }    
+}
