34c20ab06dc4d13f0f326a11da0a8b661cfeacf2
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-10-02 10:29:34 -0600

    Tab filter for the ListSchedules page in the Doctor panel

--- app/Filament/Doctor/Resources/ScheduleResource/Pages/ListSchedules.php
+++ app/Filament/Doctor/Resources/ScheduleResource/Pages/ListSchedules.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Doctor\Resources\ScheduleResource\Pages;
 
+use App\Enums\DaysOfTheWeek;
 use App\Filament\Doctor\Resources\ScheduleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
 
 class ListSchedules extends ListRecords
 {
@@ -15,5 +19,30 @@ class ListSchedules extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'Sunday' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('day_of_week', DaysOfTheWeek::Sunday)),
+            'Monday' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('day_of_week', DaysOfTheWeek::Monday)),
+            'Tuesday' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('day_of_week', DaysOfTheWeek::Tuesday)),
+            'Wednesday' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('day_of_week', DaysOfTheWeek::Wednesday)),
+            'Thursday' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('day_of_week', DaysOfTheWeek::Thursday)),
+            'Friday' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('day_of_week', DaysOfTheWeek::Friday)),
+            'Saturday' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('day_of_week', DaysOfTheWeek::Saturday)),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string | int | null
+    {
+        return Carbon::today()->format('l');
     }
 }
