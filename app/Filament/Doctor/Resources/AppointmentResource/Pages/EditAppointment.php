1310259b01962e74fafb89a90062d9a28c52d3d7
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-10-02 10:27:38 -0600

    Updates to the EditAppointment page for the Doctor panel

---
+++ app/Filament/Doctor/Resources/AppointmentResource/Pages/EditAppointment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Doctor\Resources\AppointmentResource\Pages;
+
+use App\Filament\Doctor\Resources\AppointmentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditAppointment extends EditRecord
+{
+    protected static string $resource = AppointmentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): ?string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
