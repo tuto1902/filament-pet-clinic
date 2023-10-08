12d7924ebb6747ee102373649fb3dd2b1590a700
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-10-08 12:06:33 -0600

    Refactor availbale slots

--- app/Models/Slot.php
+++ app/Models/Slot.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -24,10 +25,20 @@ class Slot extends Model
     protected function formattedTime(): Attribute
     {
         return Attribute::make(
-            get: fn ($value, array $attributes) => 
+            get: fn ($value, array $attributes) =>
                 Carbon::parse($attributes['start'])->format('h:i A') . ' - ' .
                 Carbon::parse($attributes['end'])->format('h:i A')
         );
+    }
+
+    public function scopeAvailableFor(Builder $query, User $doctor, int $dayOfTheWeek, int $clinicId): void
+    {
+        $query->whereHas('schedule', function (Builder $query) use ($doctor, $dayOfTheWeek, $clinicId) {
+            $query
+                ->where('clinic_id', $clinicId)
+                ->where('day_of_week', $dayOfTheWeek)
+                ->whereBelongsTo($doctor, 'owner');
+        });
     }
 
     public function appointment(): HasMany
