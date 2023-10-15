98f885e46c2acbfafce592c848d83725ff2be7dc
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-10-15 16:32:39 -0600

    Notes relation manager

---
+++ app/Filament/Doctor/Resources/AppointmentResource/RelationManagers/NotesRelationManager.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Doctor\Resources\AppointmentResource\RelationManagers;
+
+use App\Models\Appointment;
+use App\Models\Note;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NotesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'notes';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\RichEditor::make('body')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('id')
+            ->columns([
+                Tables\Columns\TextColumn::make('body')
+                    // ->formatStateUsing(fn ($state) => strip_tags($state))
+                    ->html()
+                    ->limit(255)
+                    ->description(fn (Note $note) => $note->created_at->format('M d, Y'))
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
