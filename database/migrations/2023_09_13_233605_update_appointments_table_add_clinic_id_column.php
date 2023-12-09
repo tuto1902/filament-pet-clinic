f4693829a815acb92a729a3fa97b8fb2e5a88938
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2023-12-08 20:32:36 -0600

    remove nullable columns from appointment table

--- database/migrations/2023_09_13_233605_update_appointments_table_add_clinic_id_column.php
+++ database/migrations/2023_09_13_233605_update_appointments_table_add_clinic_id_column.php
@@ -12,7 +12,7 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('appointments', function (Blueprint $table) {
-            $table->foreignIdFor(Clinic::class)->nullable();
+            $table->foreignIdFor(Clinic::class);
         });
     }
 
